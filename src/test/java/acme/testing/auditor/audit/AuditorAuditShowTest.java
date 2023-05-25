46fe9a3e1345b5b68141cdf2226f6698d0cec2c1
Jaime García García <jaigargar1@alum.us.es>
2023-05-25 18:41:57 +0200

    auditor changes #2

--- src/test/java/acme/testing/auditor/audit/AuditorAuditShowTest.java
+++ src/test/java/acme/testing/auditor/audit/AuditorAuditShowTest.java
@@ -1,8 +1,14 @@
 
 package acme.testing.auditor.audit;
 
+import java.util.Collection;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import acme.entities.audit.Audit;
 import acme.testing.TestHarness;
 
 public class AuditorAuditShowTest extends TestHarness {
@@ -10,55 +16,56 @@ public class AuditorAuditShowTest extends TestHarness {
 	@Autowired
 	protected AuditorAuditTestRepository repository;
 
-	//	@ParameterizedTest
-	//	@CsvFileSource(resources = "/auditor/audit/show-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-	//	public void test100positive(final int recordIndex, final String published, final String course, final String code, final String conclusion, final String strongPoints, final String weakPoints, final String mark) {
-	//
-	//		super.signIn("auditor1", "auditor1");
-	//
-	//		super.clickOnMenu("Auditor", "List my audits");
-	//		super.checkListingExists();
-	//
-	//		super.sortListing(0, "asc");
-	//
-	//		super.clickOnListingRecord(recordIndex);
-	//
-	//		super.checkInputBoxHasValue("code", code);
-	//		super.checkInputBoxHasValue("conclusion", conclusion);
-	//		super.checkInputBoxHasValue("strongPoints", strongPoints);
-	//		super.checkInputBoxHasValue("weakPoints", weakPoints);
-	//		//		super.checkInputBoxHasValue("mark", mark);
-	//		//		super.checkInputBoxHasValue("published", published);
-	//		//		super.checkInputBoxHasValue("course", course);
-	//
-	//		super.signOut();
-	//	}
 
-	//	@Test
-	//	public void test300Hacking() {
-	//		final Collection<Audit> audits;
-	//		String param;
-	//
-	//		audits = this.repository.findManyAuditsByAuditor("auditor1");
-	//		for (final Audit a : audits)
-	//			if (a.isPublished()) {
-	//				param = String.format("id=%d", a.getId());
-	//
-	//				super.checkLinkExists("Sign in");
-	//				super.request("/auditor/audit/show", param);
-	//				super.checkPanicExists();
-	//
-	//				super.signIn("administrator1", "administrator1");
-	//				super.request("/auditor/audit/show", param);
-	//				super.checkPanicExists();
-	//				super.signOut();
-	//
-	//				super.signIn("auditor2", "auditor2");
-	//				super.request("/auditor/audit/show", param);
-	//				super.checkPanicExists();
-	//				super.signOut();
-	//
-	//			}
-	//	}
+	@ParameterizedTest
+	@CsvFileSource(resources = "/auditor/audit/show-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100positive(final int recordIndex, final String published, final String course, final String code, final String conclusion, final String strongPoints, final String weakPoints, final String mark) {
+
+		super.signIn("auditor1", "auditor1");
+
+		super.clickOnMenu("Auditor", "List my audits");
+		super.checkListingExists();
+
+		super.sortListing(0, "asc");
+
+		super.clickOnListingRecord(recordIndex);
+
+		//super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("conclusion", conclusion);
+		super.checkInputBoxHasValue("strongPoints", strongPoints);
+		super.checkInputBoxHasValue("weakPoints", weakPoints);
+		//		//		super.checkInputBoxHasValue("mark", mark);
+		//		//		super.checkInputBoxHasValue("published", published);
+		//		//		super.checkInputBoxHasValue("course", course);
+
+		super.signOut();
+	}
+
+	@Test
+	public void test300Hacking() {
+		final Collection<Audit> audits;
+		String param;
+
+		audits = this.repository.findManyAuditsByAuditor("auditor1");
+		for (final Audit a : audits)
+			if (a.isPublished()) {
+				param = String.format("id=%d", a.getId());
+
+				super.checkLinkExists("Sign in");
+				super.request("/auditor/audit/show", param);
+				super.checkPanicExists();
+
+				super.signIn("administrator1", "administrator1");
+				super.request("/auditor/audit/show", param);
+				super.checkPanicExists();
+				super.signOut();
+
+				super.signIn("auditor2", "auditor2");
+				super.request("/auditor/audit/show", param);
+				super.checkPanicExists();
+				super.signOut();
+
+			}
+	}
 
 }
